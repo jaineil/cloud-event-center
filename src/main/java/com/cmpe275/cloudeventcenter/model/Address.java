1581d37d392fda40a09a9f5a9c5016bce1f38eba
jash-05 <jash.sheth@gmail.com>
2022-05-08 03:40:55 -0700

    added Event and Address models

---
+++ src/main/java/com/cmpe275/cloudeventcenter/model/Address.java
@@ -0,0 +1,34 @@
+package com.cmpe275.cloudeventcenter.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "ADDRESS")
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ADDRESS_ID")
+    private long address_id;
+
+    @Column(name = "STREET")
+    private String street;
+
+    @Column(name = "APARTMENT")
+    private String apartment;
+
+    @Column(name = "CITY")
+    private String city;
+
+    @Column(name = "STATE")
+    private String state;
+
+    @Column(name = "ZIPCODE")
+    private String zipcode;
+}
---
+++ src/main/java/com/cmpe275/cloudeventcenter/model/Event.java
@@ -0,0 +1,109 @@
+package com.cmpe275.cloudeventcenter.model;
+
+import lombok.*;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "EVENTS")
+public class Event {
+    public enum AdmissionPolicy {
+        FirstComeFirstServe,
+        OnApproval
+    }
+
+    public enum EventStatus {
+        SignUpOpen,
+        SignUpClosed,
+        Ongoing,
+        Finished,
+        Cancelled,
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "EVENT_ID")
+    private long event_id;
+
+    @Column(name = "DESCRIPTION")
+    private String description;
+
+    @Column(name = "START_TIME", columnDefinition = "TIMESTAMP")
+    private LocalDateTime startTime;
+
+    @Column(name = "END_TIME", columnDefinition = "TIMESTAMP")
+    private LocalDateTime endTime;
+
+    @Column(name = "DEADLINE", columnDefinition = "TIMESTAMP")
+    private LocalDateTime deadline;
+
+    @OneToOne
+    @JoinColumn(name = "ADDRESS_ID")
+    private Address address;
+
+    @Column(name = "MIN_PARTICIPANTS")
+    private int minParticipants;
+
+    @Column(name = "MAX_PARTICIPANTS")
+    private int maxParticipants;
+
+    @Column(name = "FEE")
+    private double fee;
+
+    @Enumerated(EnumType.STRING)
+    private AdmissionPolicy admissionPolicy;
+
+    @Enumerated(EnumType.STRING)
+    private EventStatus eventStatus;
+
+}
+
+
+//public class Player {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(name = "ID")
+//    private long id;
+//
+//    @Column(name = "FIRST_NAME", nullable = false)
+//    private String firstName;
+//
+//    @Column(name = "LAST_NAME", nullable = false)
+//    private String lastName;
+//
+//    @Column(name = "EMAIL", nullable = false, unique = true)
+//    private String email;
+//
+//    @Column(name = "DESCRIPTION")
+//    private String description;
+//
+//    @Embedded
+//    @AttributeOverrides({
+//            @AttributeOverride( name = "street", column = @Column(name = "STREET")),
+//            @AttributeOverride( name = "city", column = @Column(name = "CITY")),
+//            @AttributeOverride( name = "zip", column = @Column(name = "ZIP")),
+//            @AttributeOverride( name = "state", column = @Column(name = "STATE"))
+//    })
+//    private Address address;
+//
+//    @ManyToOne(fetch = FetchType.EAGER)
+//    @JoinColumn(name = "TEAM_ID")
+//    @JsonIgnoreProperties({"players", "address"})
+//    private Team team;
+//
+//    @ManyToMany(fetch = FetchType.EAGER)
+//    @OnDelete(action = OnDeleteAction.CASCADE)
+//    @JoinTable(name="OPPONENT",
+//            joinColumns=@JoinColumn(name="playerId"),
+//            inverseJoinColumns=@JoinColumn(name="opponentId")
+//    )
+//    @JsonIgnoreProperties({"address", "team", "opponents"})
+//    private List<Player> opponents;
+//
+//}
+
