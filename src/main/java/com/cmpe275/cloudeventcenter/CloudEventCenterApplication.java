406b0793dac088b11c4ff80d35af66050f9598f9
jaineil <mandaviajaineil@gmail.com>
2022-05-07 21:58:57 -0700

    chore: spring init

---
+++ src/main/java/com/cmpe275/cloudeventcenter/CloudEventCenterApplication.java
@@ -0,0 +1,13 @@
+package com.cmpe275.cloudeventcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudEventCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudEventCenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cmpe275/cloudeventcenter/CloudEventCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmpe275.cloudeventcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudEventCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
